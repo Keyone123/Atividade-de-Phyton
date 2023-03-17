954eaa6ed2aa0c5afbe355d562d5ab15facb0b55
Keyone123 <120115847+Keyone123@users.noreply.github.com>
2023-03-16 21:44:37 -0300

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,30 @@
+class disciplina:
+    cod = 0
+    nome = ''
+    semestre = 0
+    periodo = 0
+    carga = 0
+    aluno = []
+    professor = []
+    horario = []
+
+
+class escola:
+    lista = []
+
+
+class professor:
+    nome = ''
+    matricula = 0
+
+
+class aluno:
+    nome = ''
+    matritula = ''
+    curso = ''
+    nota1 = 0.0
+    nota2 = 0.0
+    nota3 = 0.0
+    media = 0.0
+    lista_aluno = []
+    lmedia = []
